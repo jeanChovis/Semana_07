4125a176ba508ab5ed7c010863f99348bba68dff
jeanChovis <76266001@continental.edu.pe>
2023-10-05 16:30:25 -0500

    Este es el primer commit

---
+++ src/Operaciones_Aritmeticas.py
@@ -0,0 +1,4 @@
+class Operaciones_Aritmeticas:
+    # ToDo Falta implementar la clase
+    def suma(self, sumando1, sumando2):
+        return None
---
+++ test/Prueba_Operaciones_Aritmeticas.py
@@ -0,0 +1,7 @@
+import unittest
+
+
+class Prueba_Operaciones_Aritmeticas(unittest.TestCase):
+    def test_suma_dosNumeros_respuestaSuma(self):
+        self.assertEqual(True, False)
+
